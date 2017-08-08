9dd6455aa1980a0f6e1a7603f856249d08f657e2
Omri Shaiko <omri@shaiko.com>
2017-08-08 22:38:18 +0300

    invoice summary first edition

---
+++ frontend/src/components/invoice-summary/InvoiceHeader.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="header">
+    <div class="top">
+      <div>
+        <img class="commerce-block-white-bg" src="../../assets/white-logo.png" />
+        <span class="commerce-block-green">
+          <img src="../../assets/comany-name.svg" />
+        </span>
+      </div>
+      <img class="icon-lock-green" src="../../assets/icon-lock-green.svg" />
+    </div>
+    <div class="invoice-data">
+      <div class="invoice-id">Inovice #ID-000004</div>
+      <div class="invoice-date">Date: 29/7/17</div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+</script>
+
+<style scoped lang="scss">
+  .header {
+    border-radius: 3px 3px 0 0;
+    background-color: #258C42;
+    color: #ffffff;
+    padding: 5px;
+
+    .top {
+      display: flex;
+      justify-content: space-between;
+    }
+
+    .invoice-data {
+      color: #FFFFFF;
+      font-family: "Open Sans";
+      text-align: center;
+      margin: 10px;
+
+      .invoice-id {
+        font-size: 24px;
+        font-weight: bold;
+      }
+
+      .invoice-date {
+        color: #C4E0CC;
+        font-size: 16px;
+        font-weight: 600;
+      }
+    }
+
+    .icon-lock-green {
+      /* height: 2.15%; */
+      /* width: 1.6%; */
+      /* opacity: 0.25; */
+    }
+
+    .commerce-block-green {
+      margin-top: -5px;
+    }
+
+    .commerce-block-white-bg {
+      /* height: 24px; */
+      /* width: 32px; */
+      /* opacity: 0.25; */
+    }
+  }
+
+</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import InvoiceSummary from '@/components/invoice-summary/InvoiceSummary'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/invoice-summary',
+      name: 'invoice-summary',
+      component: InvoiceSummary
     }
   ]
 })
