d2c9848cd1fcdb459704d111d28e91f8ca02b2fb
Omri Shaiko <omri@shaiko.com>
2017-08-12 23:30:57 +0300

    query invoiceByLink

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,13 +3,30 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import { ApolloClient, createBatchingNetworkInterface } from 'apollo-client'
+import VueApollo from 'vue-apollo'
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  networkInterface: createBatchingNetworkInterface({
+    uri: 'http://localhost:3000/graphql'
+  }),
+  connectToDevTools: true
+})
+
+Vue.use(VueApollo)
 
 Vue.config.productionTip = false
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient
+})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  apolloProvider,
   template: '<App/>',
   components: { App }
 })
