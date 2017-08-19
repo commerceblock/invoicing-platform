5817f3b8bbf6b7600daf95aa47f03136737b0bab
Omar Shibli <omarshib@gmail.com>
2017-08-19 08:49:51 +0300

    added EntranceModal

--- frontend/src/components/portal/Home.vue
+++ frontend/src/components/portal/Home.vue
@@ -1,19 +1,27 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Message: {{ hello }}</h2>
+  <div class="wrapper">
+    <modal v-if="showEntranceModal" @close="showEntranceModal = false" />
+    <div class="hello">
+      <h1>{{ msg }}</h1>
+      <h2>Message: {{ hello }}</h2>
+    </div>
   </div>
 </template>
 
 <script>
 import gql from 'graphql-tag'
+import Modal from './EntranceModal.vue'
 
 export default {
   name: 'hello',
+  components: {
+    Modal
+  },
   data: function () {
     return {
       msg: 'Welcome to Your Vue.js App',
-      hello: 'foo4life!'
+      hello: 'foo4life!',
+      showEntranceModal: true
     }
   },
   apollo: {
